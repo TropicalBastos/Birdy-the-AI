e6b51a1b38c589cdf982f285b2ab2a62ac9341d6
Ian Bastos <ian-bastos@live.com>
2019-07-19 21:43:58 +0100

    better text alignment formatting

--- src/core/Transition.h
+++ src/core/Transition.h
@@ -14,18 +14,22 @@ namespace birdy
         sf::Font font;
         font.loadFromFile("res/fonts/code.otf");
         parent->clear();
+
         sf::RectangleShape rect;
         sf::Vector2u windowSize = parent->getSize();
         rect.setSize(static_cast<sf::Vector2f>(windowSize));
         rect.setPosition(0, 0);
         rect.setFillColor(sf::Color(0, 0, 0, 100));
+
         sf::Text text;
         text.setString(str);
-        uint32_t textPixelSize = text.getCharacterSize() * str.length();
-        uint32_t textPixelHalfSize = textPixelSize / 2;
-        text.setPosition((windowSize.x / 2) - textPixelHalfSize / 2, windowSize.y / 2);
         text.setFillColor(sf::Color(255, 255, 255, 100));
         text.setFont(font);
+
+        uint32_t localTextBoundsWidth = text.getLocalBounds().width;
+        uint32_t localTextBoundsHeight = text.getLocalBounds().height;
+        text.setPosition((windowSize.x / 2) - localTextBoundsWidth / 2, (windowSize.y / 2) - localTextBoundsHeight / 2);
+
         parent->draw(rect);
         parent->draw(text);
         parent->display();
